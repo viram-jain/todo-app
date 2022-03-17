092b5abef69851fad7d03c5d493cf20dbfd84fea
Viram Jain <viramjain9742@gmail.com>
2022-03-17 12:26:49 +0530

    changed env and logger path

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+
 	"todoapp/logger"
 	"todoapp/router"
 
@@ -15,8 +16,8 @@ import (
 func main() {
 	sugarLogger := logger.InitLogger()
 	router.Router()
-	fmt.Printf("Listening & Serving on : %s\n", os.Getenv("SERVER_PORT"))
-	zap.L().Info(fmt.Sprintf("Listening & Serving on : %s", os.Getenv("SERVER_PORT")))
+	fmt.Printf("Listening & Serving on %s\n", os.Getenv("SERVER_PORT"))
+	zap.L().Info(fmt.Sprintf("Listening & Serving on %s", os.Getenv("SERVER_PORT")))
 	err := http.ListenAndServe(os.Getenv("SERVER_PORT"), nil)
 	if err != nil {
 		sugarLogger.Errorf("Failed to start server %s", err.Error())
--- constant/field.go
+++ constant/field.go
@@ -3,6 +3,6 @@ package constant
 const (
 	ID      = "id"
 	Key     = "_id"
-	Logfile = "/home/AD.KALEYRA.LOCAL/viram.jain/go/src/todo-app/logfile.log"
-	Envfile = "/home/AD.KALEYRA.LOCAL/viram.jain/go/src/todo-app/.env"
+	Logfile = "logfile.log"
+	Envfile = ".env"
 )
